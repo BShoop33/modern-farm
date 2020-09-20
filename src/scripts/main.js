e37d1470ac9f77837e5323e85c8bdc268b51b875
bshoop <braxton.shoop@gmail.com>
2020-09-20 14:16:01 -0500

    stuck on harvesting, need to check on the desired output.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,13 @@ console.log("Welcome to the main module")
 import { createPlan } from './plan.js'
 import { plantSeeds } from './tractor.js'
 import { usePlants } from './field.js'
+import { harvestPlants } from './harvester.js'
+
 const createdPlan = createPlan();
 plantSeeds(createdPlan);
 
 const plantsArray = usePlants();
 console.log(plantsArray);
+
+const test = harvestPlants()
+console.log(test)
